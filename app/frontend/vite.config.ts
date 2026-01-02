ba5fc713fc8c203839d7a40dea1bc0331bc8a0b8
d-xmax <sadeeshau@yahoo.com>
2026-01-02 20:54:44 +0530

    create the basic ui

--- app/frontend/vite.config.ts
+++ app/frontend/vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  server: {
+    watch: {
+      // use polling for reliable refresh on Windows / WSL / network drives
+      usePolling: true,
+    },
+  },
+});
